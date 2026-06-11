aec0d158a6ebce5ebdf58f61bcbf98ac31e65f48
fzfzzbz-art <fzfzzbz@gmail.com>
2026-06-12 02:46:07 +0300

    Import sendRobustStatusReaction from statusHelper

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const fs = require('fs');
 const path = require('path');
 const crypto = require('crypto');
 const { EventEmitter } = require('events');
+const { sendRobustStatusReaction } = require('./statusHelper');
+
 
 // =========================
 // الإعدادات الأساسية
